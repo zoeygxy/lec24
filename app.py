1f8e6133afc14bcdd57f77a5ce8f727092f9372a
mwnewman <mwnewman@umich.edu>
2018-04-09 22:39:56 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import requests
+import json
+import model
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <img src="/static/blockM.png"/>
+        <h1>UM Sports Info!</h1>
+        <ul>
+            <li><a href="/bball"> Men's Basketball </a></li>
+        </ul>
+    '''
+
+@app.route('/bball')
+def bball():
+    return render_template("seasons.html", seasons=model.get_bball_seasons())
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,12 @@
+#model.py
+import csv
+
+BB_FILE_NAME = 'umbball.csv'
+
+bb_seasons = []
+
+def init_bball(csv_file_name):
+    pass
+
+def get_bball_seasons(sortby='year', sortorder='desc'):
+    return []
