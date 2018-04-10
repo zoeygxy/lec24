83c6738fe3af2fc6e8417ea691a0b69e078b3bdf
xinyige <xinyige@umich.edu>
2018-04-10 10:18:01 -0400

    modified

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-import requests
-import json
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -15,9 +13,30 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
+@app.route('/bball', methods = ['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", sport="Men's Basketball",
+         seasons=seasons)
+
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
 
 if __name__ == '__main__':
+    model.init_bball()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -5,9 +5,31 @@ BB_FILE_NAME = 'umbball.csv'
 
 bb_seasons = []
 
-def init_bball(csv_file_name):
-    pass
+def init_bball(csv_file_name = BB_FILE_NAME):
+    global bb_seasons
+    with open('umbball.csv') as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader)
+        global bb_seasons
+        bb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[5])
+            bb_seasons.append(r)
+
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
